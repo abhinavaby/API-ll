f1aaafee55f1820e5b2924ecb7fb8fce9aaf37d1
abhinavaby <abhinavaby07@gmail.com>
2026-04-30 23:03:22 +0530

    Update api.js

--- api.js
+++ api.js
@@ -3,9 +3,9 @@ const URL="https://meowfacts.herokuapp.com";
 
 const getfacts= async ()=>{
     let response=await fetch(URL);
-    console.log(response);
+    
     let data=await response.json();
-    console.log(data.data[0]);
+    
     document.getElementById("facts").innerText=data.data[0];
     document.getElementById("a").style.display="none";
        
